2fa983c11cf4013e4206ae23da1708da8f9d9ffe
Nicholas Shewmaker <nshewmaker@hotmail.com>
2020-10-30 17:58:55 -0400

    upgrading to Rxjs

--- src/gg-api.ts
+++ src/gg-api.ts
@@ -1,13 +1,49 @@
-import { API_KEY, API_SUFFIX, API_URL } from '../api-key.secret';
-import { Endpoint, EndpointMethods } from './gg-data';
 import * as $ from 'jquery';
 import jqXHR = JQuery.jqXHR;
+import { Observable, from } from 'rxjs';
+import { filter, map, take, tap } from 'rxjs/operators';
+
+import { API_KEY, API_SUFFIX, API_URL } from '../api-key.secret';
+import { Endpoint, EndpointMethods } from './gg-data';
+
 
 export class GGApi {
     public listTransactions (params?: object): jqXHR {
         return this.callApi(Endpoint.TRANSACTION_LIST, {
             filterValue: 'Winson'
         });
+    }
+
+
+    public getTransactions (): Observable<any> {
+        return from(
+            this.callApi(Endpoint.TRANSACTION_LIST)
+        ).pipe(
+            map((response) => response.forms[0].transactions),
+            tap((_) => console.log('transactions', _) ),
+            // filter((transaction) => transaction.transStatus === 'Accepted'),
+            map((transactions) => {
+                const rv: object[] = [];
+                transactions.filter((record: object) => {
+                    return record.transStatus === 'Accepted';
+                }).forEach((record: object) => {
+                    const obj = {
+                        // TODO: create interfaces for gg responses
+                        id:        record.id,
+                        name:      `${record.firstName} ${record.lastName}`,
+                        anonymous: record.transactionWasAnonymous === 'y',
+                        memo:      record.transactionMemo || null,
+                        location:  `${record.billingCity}, ${record.billingState}`,
+                        amount:    record.value,
+                    };
+
+                    rv.push(obj);
+                });
+
+                return rv;
+            }),
+            take(1),
+        );
     }
 
     public callApi (endpoint: Endpoint, params?: object): jqXHR {
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,14 @@ import * as $ from 'jquery';
 // TODO: figure out why baseUrl imports aren't working (https://www.google.com/search?hl=en&q=typescript%20webpack%20import%20%22resolve%20as%20module%22%20baseUrl)
 import { GGApi } from './gg-api';
 import { Endpoint } from './gg-data';
+import { Tent } from './tent';
 
 $((event) => {
     const outputJQO = $('pre#output');
     const myApi = new GGApi();
+
+    // const myTent = new Tent();
+    // myTent.poll(Endpoint.EVENT_LIST, 500).subscribe();
 
     // myApi.callApi(Endpoint.PLEDGE_LIST, {
     //     dateBegin: '2020-07-07',
@@ -24,7 +28,13 @@ $((event) => {
     //     outputJQO.html(JSON.stringify(result, null, 2));
     // });
 
-    myApi.listTransactions().done((result) => {
-        outputJQO.html(JSON.stringify(result, null, 2));
-    });
+    // myApi.listTransactions().done((result) => {
+    //     outputJQO.html(JSON.stringify(result, null, 2));
+    // });
+
+    myApi.getTransactions().subscribe(
+        (result) => {
+            outputJQO.html(JSON.stringify(result, null, 2));
+        }
+    );
 });
