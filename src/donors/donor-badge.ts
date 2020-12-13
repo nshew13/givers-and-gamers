b1244507694585328a5151e60670745b3295de4b
Nicholas Shewmaker <nshewmaker@hotmail.com>
2020-12-12 19:26:01 -0500

    externalized DonorBadge class

---
+++ src/donors/donor-badge.ts
@@ -0,0 +1,74 @@
+import { IDonation } from 'qgiv/qgiv.interface';
+
+
+export class DonorBadge {
+    private _badgeEl: HTMLDivElement;
+
+    /**
+     * cumulative time of all transitions to show, in milliseconds
+     *
+     * This is $durationSpin + $durationFade.
+     */
+    public static readonly ANIMATION_DURATION_MSEC = 1300;
+    /**
+     * length of time between fade in and fade out
+     */
+    public static readonly SHOW_DURATION_MSEC = 4000;
+
+    private static _HTML_BODY: HTMLBodyElement;
+    private static _HTML_TEMPLATE: HTMLTemplateElement;
+
+    public constructor (donation: IDonation) {
+        const badgeTpl = document.importNode(DonorBadge._HTML_TEMPLATE.content, true);
+        this._badgeEl = badgeTpl.querySelector('div.donation');
+
+        badgeTpl.querySelector('div.donor > p.name').textContent = donation.displayName;
+        badgeTpl.querySelector('div.donor > p.loc').textContent = donation.displayName;
+        console.log('appending badge');
+        DonorBadge._HTML_BODY.appendChild(badgeTpl);
+
+        this._restyle();
+    }
+
+    public get element (): HTMLDivElement {
+        return this._badgeEl;
+    }
+
+    public show () {
+        console.log('showing badge');
+        this._badgeEl.classList.add('show', 'expand');
+        this._restyle();
+    }
+
+    public hide (remove: boolean = false) {
+        console.log('hiding badge');
+        this._badgeEl.classList.remove('show');
+        this._restyle();
+
+        if (remove) {
+            setTimeout(_ => { this._badgeEl.remove(); },
+                DonorBadge.ANIMATION_DURATION_MSEC
+            );
+        }
+    }
+
+    private _restyle () {
+        // force the browser to calculate the styles of the new badge
+        // https://stackoverflow.com/a/6918307/356016
+        window.getComputedStyle(this._badgeEl).getPropertyValue('top');
+    }
+
+    public static init () {
+        if (document.readyState === 'loading') {
+            // wait for DOM to be ready
+            document.addEventListener('DOMContentLoaded', DonorBadge._onReady);
+        } else {
+            DonorBadge._onReady();
+        }
+    }
+
+    private static _onReady () {
+        DonorBadge._HTML_BODY = document.getElementsByTagName('body')[0];
+        DonorBadge._HTML_TEMPLATE = document.getElementById('donorBadgeTpl') as HTMLTemplateElement;
+    }
+}
--- src/donors/donors.ts
+++ src/donors/donors.ts
@@ -2,85 +2,13 @@ import { of } from 'rxjs';
 import { concatMap, delay, first, tap } from 'rxjs/operators';
 // import { differenceInMilliseconds, toDate, parse, parseJSON } from 'date-fns';
 
-import { QGiv } from 'qgiv/qgiv';
-import { IDonation, ITransaction } from 'qgiv/qgiv.interface';
 import { GGFeed } from 'mock/gg-feed-mock';
+import { IDonation } from 'qgiv/qgiv.interface';
+import { QGiv } from 'qgiv/qgiv';
 import { Utilities } from 'utilities';
 
+import { DonorBadge } from './donor-badge';
 import './donors.scss';
-
-
-class DonorBadge {
-    private _badgeEl: HTMLDivElement;
-
-    /**
-     * cumulative time of all transitions to show, in milliseconds
-     *
-     * This is $durationSpin + $durationFade.
-     */
-    public static readonly ANIMATION_DURATION_MSEC = 1300;
-    /**
-     * length of time between fade in and fade out
-     */
-    public static readonly SHOW_DURATION_MSEC = 4000;
-
-    private static _HTML_BODY: HTMLBodyElement;
-    private static _HTML_TEMPLATE: HTMLTemplateElement;
-
-    public constructor (donation: IDonation) {
-        const badgeTpl = document.importNode(DonorBadge._HTML_TEMPLATE.content, true);
-        this._badgeEl = badgeTpl.querySelector('div.donation');
-
-        badgeTpl.querySelector('div.donor > p.name').textContent = donation.displayName;
-        badgeTpl.querySelector('div.donor > p.loc').textContent = donation.displayName;
-        console.log('appending badge');
-        DonorBadge._HTML_BODY.appendChild(badgeTpl);
-
-        this._restyle();
-    }
-
-    public get element (): HTMLDivElement {
-        return this._badgeEl;
-    }
-
-    public show () {
-        console.log('showing badge');
-        this._badgeEl.classList.add('show', 'expand');
-        this._restyle();
-    }
-
-    public hide (remove: boolean = false) {
-        console.log('hiding badge');
-        this._badgeEl.classList.remove('show');
-        this._restyle();
-
-        if (remove) {
-            setTimeout(_ => { this._badgeEl.remove(); },
-                DonorBadge.ANIMATION_DURATION_MSEC
-            );
-        }
-    }
-
-    private _restyle () {
-        // force the browser to calculate the styles of the new badge
-        // https://stackoverflow.com/a/6918307/356016
-        window.getComputedStyle(this._badgeEl).getPropertyValue('top');
-    }
-
-    public static init () {
-        if (document.readyState === 'loading') {
-            // wait for DOM to be ready
-            document.addEventListener('DOMContentLoaded', DonorBadge._onReady);
-        } else {
-            DonorBadge._onReady();
-        }
-    }
-
-    private static _onReady () {
-        DonorBadge._HTML_BODY = document.getElementsByTagName('body')[0];
-        DonorBadge._HTML_TEMPLATE = document.getElementById('donorBadgeTpl') as HTMLTemplateElement;
-    }
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const qgiv = new QGiv();
