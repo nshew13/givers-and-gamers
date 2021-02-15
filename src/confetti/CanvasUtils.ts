6b325d58a974cb3f0f62ffbed0ae57d8bf543d8f
Nicholas Shewmaker <nshewmaker@hotmail.com>
2021-02-15 11:23:19 -0500

    split up confetti classes

---
+++ src/confetti/CanvasUtils.ts
@@ -0,0 +1,83 @@
+export class Coord {
+    private _x: number;
+    get x (): number { return this._x; }
+    set x (val: number) { this._x = val; }
+
+    private _y: number;
+    get y (): number { return this._y; }
+    set y (val: number) { this._y = val; }
+
+    private _z: number;
+    get z (): number { return this._z; }
+    set z (val: number) { this._z = val; }
+
+    constructor (x = 0, y = 0, z = 0) {
+        this._x = x;
+        this._y = y;
+        this._z = z;
+    }
+}
+
+export class Canvas2D {
+    private _canvasEl: HTMLCanvasElement;
+    private _canvasContext: CanvasRenderingContext2D;
+
+    get context (): CanvasRenderingContext2D {
+        return this._canvasContext;
+    }
+    get height (): number {
+        return this._canvasEl.clientHeight;
+    }
+    get width (): number {
+        return this._canvasEl.clientWidth;
+    }
+
+    constructor (canvasId: string) {
+        this._canvasEl = document.getElementById(canvasId) as HTMLCanvasElement;
+        this._canvasContext = this._canvasEl.getContext('2d');
+        this.resize();
+
+        window.onresize = () => {
+            this.resize();
+        };
+    }
+
+    /**
+     * set canvas to fill entire parent element
+     */
+    public resize (): void {
+        this._canvasEl.setAttribute('width',  this._canvasEl.parentElement.clientWidth  + 'px');
+        this._canvasEl.setAttribute('height', this._canvasEl.parentElement.clientHeight + 'px');
+    }
+}
+
+/**
+ * easing equations from http://gizma.com/easing/
+ * t = current time
+ * b = start value
+ * c = delta value
+ * d = duration
+ */
+export class Ease {
+    // public static inCubic (t: number, b: number, c: number, d: number): number {
+    //     t /= d;
+    //     return c*t*t*t + b;
+    // }
+
+    public static outCubic (t: number, b: number, c: number, d: number): number {
+        t /= d;
+        t--;
+        return c*(t*t*t + 1) + b;
+    }
+
+    // public static inOutCubic (t: number, b: number, c: number, d: number): number {
+    //     t /= d/2;
+    //     if (t < 1) return c/2*t*t*t + b;
+    //     t -= 2;
+    //     return c/2*(t*t*t + 2) + b;
+    // }
+
+    // public static inBack (t: number, b: number, c: number, d: number, s = 1.70158): number {
+    //     return c*(t/=d)*t*((s+1)*t - s) + b;
+    // }
+}
