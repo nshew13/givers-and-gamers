6f631e8a99c556dd26e447ff1995838a9599d84e
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-01-24 20:13:21 -0500

    add Sass

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import './style.css'
+// import "./style.scss";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+export const KEY = "foo";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    console.log("got here");
+  }, 10_000);
+});
