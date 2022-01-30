710b65516fca777c611c8eef53ff172b8e4e9809
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-01-29 20:19:54 -0500

    added round (card) selectors

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # givers-and-gamers-golf-invitational
 
 - [Prerequisites](#prerequisites)
+- [Attribution](#attribution)
 - [Use](#use)
   - [Install dependencies](#install-dependencies)
   - [Run local dev server](#run-local-dev-server)
@@ -8,6 +9,10 @@
 
 ## Prerequisites
 * [Node](https://nodejs.org/en/download/) v16 LTS or later
+
+
+## Attribution
+<a href="https://www.flaticon.com/free-icons/golf" title="golf icons">Golf icons created by Talha Dogar - Flaticon</a>
 
 ## Use
 All commands are expected to be run from the project root directory
@@ -26,4 +31,9 @@ Browse to http://localhost:3000/ to see the app.
 
 ## Under the hood
 
-*Vite* used because of [an issue](https://github.com/parcel-bundler/parcel/issues/7574) in Parcel 2.2.1
+*Vite* used as a builder because of
+[an issue](https://github.com/parcel-bundler/parcel/issues/7574) in Parcel
+2.2.1.
+
+File viewing requires a local server, because static file opening creates
+XSRF issues with `script type="module"` (regardless of builder).
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,63 @@
 export const KEY = "foo";
 
+type SpreadsheetDef = {
+  gid: string;
+  width: string;
+};
+
+const SPREADSHEET_URL =
+  "https://docs.google.com/spreadsheets/d/19t-y9tzXteBJf3nv9m_roSUSC2lKtEl7WQaQa9uSnRc/htmlembed/sheet?gid=";
+
+const SPREADSHEETS: Array<SpreadsheetDef> = [
+  {
+    // Finals
+    gid: "1756187223",
+    width: "496px",
+  },
+  {
+    // Round 1
+    gid: "1403145623",
+    width: "1080px",
+  },
+  {
+    // Round 2
+    gid: "1287803979",
+    width: "1080px",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   const iframe = document.getElementById(
     "GoogleScoreCard"
   ) as HTMLIFrameElement;
-  const iframeSrc = iframe?.src;
+  iframe.src = SPREADSHEET_URL + SPREADSHEETS[1].gid;
+  let iframeSrc = iframe?.src;
 
   setInterval(() => {
     console.log("refreshing iframe");
     // refresh with a cache buster
     iframe.src = iframeSrc + "&cb=" + Math.ceil(Math.random() * 10_000);
   }, 10_000);
+
+  const selectSheet = (event: Event) => {
+    const selection: number = parseInt(
+      (event?.currentTarget as HTMLElement)?.dataset?.selection || "1",
+      10
+    );
+
+    iframe.src = SPREADSHEET_URL + SPREADSHEETS[selection].gid;
+    iframeSrc = iframe?.src;
+    document.documentElement.style.setProperty(
+      "--iframe-width",
+      SPREADSHEETS[selection].width
+    );
+  };
+
+  // bind onclick events to selector controls
+  const selectors: NodeList = document.querySelectorAll(
+    "div.round-selector > div"
+  );
+  selectors.forEach((el) => {
+    el.addEventListener("click", selectSheet, true);
+  });
 });
