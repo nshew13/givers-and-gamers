416e4808e0dd6c14f05b662722f36aa31d5cfa75
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-02-08 23:00:38 -0500

    mostly-working, multi-page Vite; though server still needs "pages/" in URL

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,20 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// TODO: set base using env dev/prod (https://stackoverflow.com/a/69041080/356016)
+
 // vite.config.js
-export default {
-    base: '/static/gng-golf/',
-}
+export default defineConfig({
+    // base: '/static/gg/',
+    // root: path.resolve(__dirname, 'src'),
+    // publicDir: path.resolve(__dirname, 'public'),
+    // https://stackoverflow.com/a/70523299/356016
+    build: {
+        rollupOptions: {
+            input: {
+                golf: path.resolve(__dirname, 'pages/golf/index.html'),
+                placeholder: 'index.html',
+            },
+        },
+    },
+});
