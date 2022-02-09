87d67fa8989ec4f4ea1f77b7ca61e3ca297a0ba8
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-02-09 18:14:29 -0500

    added vite-tsconfig-paths

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite';
 
 // TODO: set base using env dev/prod (https://stackoverflow.com/a/69041080/356016)
 
 // vite.config.js
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     // base: '/static/gg/',
-    // root: path.resolve(__dirname, 'src'),
+    // root: path.resolve(__dirname),
     // publicDir: path.resolve(__dirname, 'public'),
     // https://stackoverflow.com/a/70523299/356016
     // The build inputs only affect the build target, not the local server.
@@ -17,7 +19,7 @@ export default defineConfig({
                 schedule: path.resolve(__dirname, 'elements/schedule/index.html'),
                 leaderboard: path.resolve(__dirname, 'elements/leaderboard/index.html'),
                 progress: path.resolve(__dirname, 'elements/progress/index.html'),
-                placeholder: 'index.html',
+                placeholder: path.resolve(__dirname, 'index.html'),
             },
         },
     },
