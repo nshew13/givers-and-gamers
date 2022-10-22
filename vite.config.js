c4b517beea90d5b33a18ca4a873a7d52c430f2f1
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-10-22 00:13:21 -0400

    working glob for all Eleventy-built HTML

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+import glob from 'glob';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite';
 
 // TODO: set base using env dev/prod (https://stackoverflow.com/a/69041080/356016)
 
-// TODO: find a way to share this config
-const DIR_INTERMEDIATE = path.resolve(__dirname, 'src');
+// TODO: find a way to share this value
+const DIR_INTERMEDIATE = 'src';
+
+// based on https://rollupjs.org/guide/en/#input
+const allHtmlEntires = glob.sync(`${DIR_INTERMEDIATE}/**/*.html`).map(file =>
+    fileURLToPath(new URL(file, import.meta.url))
+);
+allHtmlEntires.push(path.resolve(__dirname, 'index.html'));
+
 
 export default defineConfig({
     plugins: [
@@ -19,13 +28,7 @@ export default defineConfig({
     // N.B.: The build inputs below only affect the "build" target, NOT THE LOCAL SERVER ("start" target).
     build: {
         rollupOptions: {
-            input: {
-                golf: path.resolve(DIR_INTERMEDIATE, 'golf/index.html'),
-                schedule: path.resolve(DIR_INTERMEDIATE, 'schedule/index.html'),
-                leaderboard: path.resolve(DIR_INTERMEDIATE, 'leaderboard/index.html'),
-                progress: path.resolve(DIR_INTERMEDIATE, 'progress/index.html'),
-                placeholder: path.resolve(__dirname, 'index.html'),
-            },
+            input: allHtmlEntires,
         },
     },
 });
