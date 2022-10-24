838164ce93e81b0bbe6d1e62e7817bb116aa4969
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-10-24 15:23:29 -0400

    header scrolling adjustments

--- libs/index.ts
+++ libs/index.ts
@@ -1,8 +1,10 @@
 export {}
 
+const SHRINK_HEADER_AT_SCROLL_POS = 10;
+
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 10) {
+        if (window.scrollY > SHRINK_HEADER_AT_SCROLL_POS) {
             document.getElementsByTagName('header')[0].classList.add('shrink');
         } else {
             document.getElementsByTagName('header')[0].classList.remove('shrink');
