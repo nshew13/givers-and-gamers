2ac6453fe55dde50fa5589f0ecede9576a841022
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-10-24 20:02:49 -0400

    apply "shrink" to whole body

--- libs/index.ts
+++ libs/index.ts
@@ -5,9 +5,9 @@ const SHRINK_HEADER_AT_SCROLL_POS = 10;
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         if (window.scrollY > SHRINK_HEADER_AT_SCROLL_POS) {
-            document.getElementsByTagName('header')[0].classList.add('shrink');
+            document.getElementsByTagName('body')[0].classList.add('shrink');
         } else {
-            document.getElementsByTagName('header')[0].classList.remove('shrink');
+            document.getElementsByTagName('body')[0].classList.remove('shrink');
         }
     });
 });
