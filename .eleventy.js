d52fc5416aa340aa91d0f7225f720a2742bb719a
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-10-27 10:47:07 -0400

    first pass with vite-plugin-eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,13 @@
 const toml = require('@iarna/toml');
 const { dateFilter } = require('./libs/utilities/NunjucksDateFilter')
 
-const DIR_INTERMEDIATE = 'src';
+// const DIR_INTERMEDIATE = 'src';
 
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('templates/**/*.css');
     eleventyConfig.addPassthroughCopy('templates/**/*.html');
     eleventyConfig.addPassthroughCopy('templates/**/*.js');
     eleventyConfig.addPassthroughCopy('templates/**/*.scss');
-    eleventyConfig.addPassthroughCopy('templates/**/*.ts');
-
     eleventyConfig.addPassthroughCopy('templates/**/*.ts');
 
     // add custom date filter
@@ -19,10 +17,11 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addDataExtension('toml', contents => toml.parse(contents));
 
     return {
-        dir: {
-            input: 'templates',
-            output: DIR_INTERMEDIATE,
-        },
+        // overridden by vite-plugin-eleventy and vite.config.js
+        // dir: {
+        //     input: 'templates',
+        //     output: DIR_INTERMEDIATE,
+        // },
         templateFormats: ['njk'],
     };
 };
--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,33 @@ import { fileURLToPath } from 'url';
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite';
 import { ViteToml } from 'vite-plugin-toml'
+const { eleventyPlugin } = require('vite-plugin-eleventy');
 
 // TODO: set base using env dev/prod (https://stackoverflow.com/a/69041080/356016)
 
 // TODO: find a way to share this value
 const DIR_INTERMEDIATE = 'src';
 
+// automatically add all HTML under DIR_INTERMEDIATE
 // based on https://rollupjs.org/guide/en/#input
-const allHtmlEntires = glob.sync(`${DIR_INTERMEDIATE}/**/*.html`).map(file =>
-    fileURLToPath(new URL(file, import.meta.url))
-);
-allHtmlEntires.push(path.resolve(__dirname, 'index.html'));
+// const allHtmlEntires = glob.sync(`${DIR_INTERMEDIATE}/**/*.html`).map(file =>
+//     fileURLToPath(new URL(file, import.meta.url))
+// );
+// allHtmlEntires.push(path.resolve(__dirname, 'index.html'));
 
 
 export default defineConfig({
     plugins: [
         tsconfigPaths(),
         ViteToml(),
+        eleventyPlugin({
+            replace: [
+                // [viteConfig.root, ''],
+                [`/${DIR_INTERMEDIATE}`, ''],
+                ['/templates', ''],
+                ['/index.html', ''],
+            ]
+        }),
     ],
     // base: '/static/gg/', // when hosted on WordPress
     // root: path.resolve(__dirname),
@@ -30,7 +40,8 @@ export default defineConfig({
     // N.B.: The build inputs below only affect the "build" target, NOT THE LOCAL SERVER ("start" target).
     build: {
         rollupOptions: {
-            input: allHtmlEntires,
+            // input: allHtmlEntires,
+            input: './index.html',
         },
     },
 });
