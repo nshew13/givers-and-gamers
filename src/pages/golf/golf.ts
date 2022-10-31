1c3b304704f449bc26c16b319b28497c4a099971
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-10-30 23:02:21 -0400

    converted golf

---
+++ src/pages/golf/golf.ts
@@ -0,0 +1,84 @@
+export {};
+
+type SpreadsheetDef = {
+    gid: string;
+    width: string;
+};
+
+const SPREADSHEET_URL =
+    "https://docs.google.com/spreadsheets/d/19t-y9tzXteBJf3nv9m_roSUSC2lKtEl7WQaQa9uSnRc/htmlembed/sheet?gid=";
+
+const SPREADSHEETS: Array<SpreadsheetDef> = [
+    {
+        // Final
+        gid: "1756187223",
+        width: "540px",
+    },
+    {
+        // Round 1
+        gid: "1403145623",
+        width: "1126px",
+    },
+    {
+        // Round 2
+        gid: "1287803979",
+        width: "1126px",
+    },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const iframe = document.getElementById(
+        "GoogleScoreCard"
+    ) as HTMLIFrameElement;
+    iframe.src = SPREADSHEET_URL + SPREADSHEETS[1]?.gid;
+    let iframeSrc = iframe?.src;
+
+    const iframeDisplay = document.querySelector(
+        "div.score-card-container"
+    ) as HTMLElement;
+
+    /**
+     * When the iframe is loaded, re-show the container by raising
+     * it's opacity.
+     */
+    const loadFrame = () => {
+        iframeDisplay.style.opacity = "1";
+    };
+    iframe.addEventListener("load", loadFrame, true);
+
+    setInterval(() => {
+        console.log("refreshing iframe");
+        // https://stackoverflow.com/a/821373/356016
+        // include a cache buster at the end of the URL
+        iframe.contentWindow?.location.replace(
+            iframeSrc + "&cb=" + Math.ceil(Math.random() * 10_000)
+        );
+    }, 10_000);
+
+    const selectSheet = (event: Event) => {
+        // begin to hide, based on transition duration
+        iframeDisplay.style.opacity = "0";
+
+        const selection: number = parseInt(
+            (event?.currentTarget as HTMLElement)?.dataset?.selection || "1",
+            10
+        );
+
+        setTimeout(() => {
+            iframe.src = SPREADSHEET_URL + SPREADSHEETS[selection]?.gid;
+            iframeSrc = iframe?.src;
+            document.documentElement.style.setProperty(
+                "--iframe-width",
+                SPREADSHEETS[selection]?.width || "0"
+            );
+        }, 100); // delay should be the same as the transition duration
+    };
+
+    // bind onclick events to selector controls
+    const selectors: NodeList = document.querySelectorAll(
+        "div.round-selector > div"
+    );
+    selectors.forEach((el) => {
+        el.addEventListener("click", selectSheet, true);
+    });
+});
