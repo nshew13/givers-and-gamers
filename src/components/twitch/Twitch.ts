68c94097b1226bb8e389f2fe424b9886c552744e
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-11-22 15:42:06 -0500

    added Streaming Now indicator (WIP)

--- src/components/twitch/Twitch.ts
+++ src/components/twitch/Twitch.ts
@@ -1,48 +1,50 @@
-import type { TwitchAccessToken, TwitchStreamsResponse } from './types';
+import type { TwitchAccessToken, TwitchStreamsData, TwitchStreamsResponse } from './types';
 
 export class Twitch {
+    private static readonly _CLIENT_ID = 'ojbmnmm8my0gb7mkb0k6rv4ime0pv4';
     private static _ACCESS_TOKEN: TwitchAccessToken;
 
-    public static async getAccessToken(): Promise<TwitchAccessToken | void> {
+    private static async _fetchAccessToken(force = false): Promise<void> {
         const headers = new Headers({
             "Content-Type": "text/json",
         });
 
-        return fetch('https://giversandgamers.org/server/twitch.php', { method: 'GET', headers }).then(
-            (response: Response) => {
-                if (response.ok) {
-                    return Promise.resolve(
-                        response.json().then((json) => {
-                            Twitch._ACCESS_TOKEN = json;
-                        })
-                    );
-                } else {
-                    return Promise.reject("Response failed");
-                }
-            }
-        );
+        if (Twitch._ACCESS_TOKEN?.access_token && !force) {
+            return Promise.resolve();
+        }
+
+        const response = await fetch('https://giversandgamers.org/server/twitch.php', { method: 'GET', headers });
+        if (response.ok) {
+            return response.json().then((json) => {
+                Twitch._ACCESS_TOKEN = json;
+            }).finally(() => {
+                Promise.resolve();
+            });
+        } else {
+            return Promise.reject("Response failed");
+        }
     }
 
-    public static async isCurrentlyStreaming(users: Array<string>): Promise<boolean | void> {
+    private static async _fetchStreams(users: Array<string>): Promise<Array<TwitchStreamsData>> {
         const headers = new Headers({
             "Content-Type": "text/json",
             Authorization: `Bearer ${Twitch._ACCESS_TOKEN.access_token}`,
+            'Client-ID': Twitch._CLIENT_ID,
         });
 
         const userParams = users.map((user) => `user_login=${user}`).join('&');
 
-        return fetch(`https://api.twitch.tv/helix/streams?${userParams}`, { method: 'GET', headers }).then(
-            (response: Response) => {
-                 if (response.ok) {
-                    return Promise.resolve(
-                        response.json().then((json: TwitchStreamsResponse) => {
-                            return !!json.data.length;
-                        })
-                    );
-                } else {
-                    return Promise.reject("Response failed");
-                }
-            }
-        );
+        const response = await fetch(`https://api.twitch.tv/helix/streams?${userParams}`, { method: 'GET', headers });
+        if (response.ok) {
+            return response.json().then((json: TwitchStreamsResponse) => json.data);
+        } else {
+            return Promise.reject("Response failed");
+        }
+    }
+
+    public static async isCurrentlyStreaming(users: Array<string>): Promise<boolean> {
+        await Twitch._fetchAccessToken();
+        const streamsData = await Twitch._fetchStreams(users);
+        return !!streamsData.length;
     }
 }
